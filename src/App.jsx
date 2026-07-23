21ea6a720b2271daae8ac99d470e8d190e637190
Juan Antonio Vicente <juanantoniovicenteruiz@hotmail.com>
2026-07-23 20:05:48 +0200

    Render lists from state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import "./App.css";
-import Summary from "./components/Summary.jsx"; 
+import Summary from "./components/Summary.jsx";
 
 function App() {
+  const [listas, setListas] = useState([
+    { id: 1, nombre: "Veces que no quiere", color: "green" },
+    { id: 2, nombre: "Cozitas", color: "red" },
+  ]);
   return (
     <>
       <section id="center">
         <div>
           <h1>Is She Right?</h1>
+          <p>Tienes {listas.length} listas</p>
+          {listas.map((lista) => (
+            <p key={lista.id}>{lista.nombre}</p>
+          ))}
         </div>
         <Summary />
       </section>

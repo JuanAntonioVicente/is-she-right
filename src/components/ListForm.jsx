04669cb54c141e7170a3c2888f8e6fbe0bf4bd2a
Juan Antonio Vicente <juanantoniovicenteruiz@hotmail.com>
2026-08-19 18:21:15 +0200

    Add form validation and reset

--- src/components/ListForm.jsx
+++ src/components/ListForm.jsx
@@ -3,11 +3,18 @@ import { useState } from "react";
 function ListForm({ onCrear }) {
     const [nombre, setNombre] = useState("");
     const [color, setColor] = useState("#008000");
+    function handleSubmit(){
+        if (nombre.trim() === "") {
+            return;
+        }
+        onCrear(nombre, color);
+        setNombre("");
+    }
     return (
         <div>
             <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
             <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
-            <button onClick={() => onCrear(nombre,color)}>Crear lista</button>
+            <button onClick={handleSubmit} >Crear lista</button>
         </div>
     )
 }
